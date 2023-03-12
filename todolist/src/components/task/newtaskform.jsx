c5887ccb670ad9a9ed71c2b7b760a60455f02bdf
hiem55 <100002888+hiem55@users.noreply.github.com>
2023-03-11 17:00:54 -0800

    Add form parameters

--- todolist/src/components/task/newtaskform.jsx
+++ todolist/src/components/task/newtaskform.jsx
@@ -1,11 +1,34 @@
+import Card from '../ui/card';
+import classes from './newtaskform.module.css';
+
 function NewTaskForm() {
-    return
-    <form className={classes.form}>
-        <div className={classes.control}>
-            <label htmlFor='title'>Task Title</label>
-            <input type='text' required id='title' />
-        </div>
-    </form>
+    // get current date in Day Month Date Year format
+    const currentDate = new Date().toDateString();
+    return (
+        <Card>
+            <form className={classes.form}>
+                <div className={classes.control}>
+                    <label htmlFor='title'>Task Title</label>
+                    <input type='text' required id='title' />
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor='description'>Description</label>
+                    <textarea id='description' required rows='5'></textarea>
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor='current-date'>Current Date</label>
+                    <input type='text' id='current-date' defaultValue={currentDate} disabled />
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor='due-date'>Due Date</label>
+                    <input type='datetime-local' id='due-date' required />
+                </div>
+                <div className={classes.actions}>
+                    <button>Add Task</button>
+                </div>
+            </form>
+        </Card>
+    );
 }
 
 export default NewTaskForm;
--- todolist/src/pages/submission.jsx
+++ todolist/src/pages/submission.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import NewTaskForm from '../components/task/newtaskform';
 
 const Submission = () => {
     return (
-        <header>
-            <h1>Something</h1>
-        </header>
+        <section>
+            <h1>Add New Task</h1>
+            <NewTaskForm />
+        </section>
     )
 }
 export default Submission
